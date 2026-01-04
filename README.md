eef28878f0ed4f834b3b5d1ec8fe9af16f331d54
Mahfuj Abdulla <155483252+IamFreak10@users.noreply.github.com>
2026-01-05 03:55:26 +0600

    Added Everything

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Problem-solving-functions
+#  Interview Questions Ans:
+# What are some differences between interfaces and types in TypeScript?
+Ans:
+১.ইন্টারফেস টাইপ ইউনিয়ন বা ইন্টারসেকশন করতে পারে না,অন্যদিকে টাইপ ইউনিয়ন বা ইন্টারসেকশন ব্যাবহার করে আরো ফেলেক্সিবল ভাবে ব্যাবহার করা যায়
+২.ইন্টারফেস দিয়ে  Class,Object এর structure এনফোরস করা যায় কিন্তু tupple,aliasunion ইউজ করতে পারে না,যেটা type আরও flexible, union, tuple, primitive alias, function signature সব করতে পারে।
+৩.Interface একই নাম দিয়ে একাধিকবার declare করলে TypeScript এগুলোকে merge করে এবং combined structure তৈরি করে।type একবার declare করার পর আর একই নাম দিয়ে declare করলে error দেয়, merge করা যায় না।
 
+# What is the use of enums in TypeScript? Provide an example of a numeric and string enum.
+Ans:
+TypeScript এ enum একটা namedd constant এর সেট,যা কোডকে readable এবং maintainable করে।এখানে আমি কম্পাইলারকে বলে দিবো যে কোন ভ্যালু গুলা এলাউড
+Example:
+Numeric Enum:
+enum Direction {
+  Up,      // 0
+  Down,    // 1
+  Left,    // 2
+  Right    // 3
+}
+
+let move: Direction = Direction.Left;
+console.log(move); // Output: 2
+
+String Enum:
+enum Status {
+  Pending = "PENDING",
+  Success = "SUCCESS",
+  Failed = "FAILED"
+}
+
+let currentStatus: Status = Status.Success;
+console.log(currentStatus); // Output: SUCCESS
--- solution.ts
+++ solution.ts
@@ -29,11 +29,6 @@ class Person {
     return `Name: ${this.name}, Age: ${this.age}`;
   }
 }
-const books = [
-  { title: 'Book A', rating: 4.5 },
-  { title: 'Book B', rating: 3.2 },
-  { title: 'Book C', rating: 5.0 },
-];
 
 type BookObj = {
   title: string;
@@ -42,17 +37,17 @@ type BookObj = {
 const filterByRating = (books: BookObj[]): BookObj[] | undefined => {
   return books.filter((book) => book.rating >= 4);
 };
-// console.log(filterByRating(books));
-const users = [
-  { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
-  { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
-  { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
-];
+
+
+
 type User = { id: number; name: string; email: string; isActive: boolean };
 const filterActiveUsers = (users: User[]): User[] => {
   return users.filter((user) => user.isActive);
 };
-// console.log(filterActiveUsers(users));
+
+
+
+
 interface Book {
   title: string;
   author: string;
@@ -66,18 +61,9 @@ const printBookDetails = (book: Book): void => {
     }, Available:${book.isAvailable ? 'Yes' : 'NO'}`
   );
 };
-const myBook: Book = {
-  title: 'The Great Gatsby',
-  author: 'F. Scott Fitzgerald',
-  publishedYear: 1925,
-  isAvailable: true,
-};
 
-// printBookDetails(myBook);
 
-// const getUniqueValues=(arr1:(number | string)[],arr2:(number | string)[])=>{
-//   const hash
-// }
+
 
 const getUniqueValues=(arr1: (number | string)[], arr2: (number | string)[]): (number | string)[] =>{
 const uniqueArr: (number | string)[] = [];
@@ -98,11 +84,10 @@ hasedTable(arr2);
 return uniqueArr
 
 }
-// const array1 = [1, 2, 3, 4, 5];
-// const array2 = [3, 4, 5, 6, 7];
-// const array1 = ['a', 'b', 'c', 'd', 'e'];
-// const array2 = ['c', 'd', 'e', 'f', 'g'];
-// console.log(getUniqueValues(array1, array2));
+
+
+
+
 
 type Products={
   name: string;
@@ -122,10 +107,3 @@ const calculateTotalPrice=(products:Products[]):number=>{
     }
   },0)
 }
-const products = [
-  { name: 'Pen', price: 10, quantity: 2 },
-  { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
-  { name: 'Bag', price: 50, quantity: 1, discount: 20 },
-];
-
-console.log(calculateTotalPrice(products));
